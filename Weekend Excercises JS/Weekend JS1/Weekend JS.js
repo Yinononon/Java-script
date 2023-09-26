b152e0f059c74b37261dff8365e77ff9d0f21280
Yinononon <yinononon@gmail.com>
2023-09-26 10:16:38 +0300

    Ex2.1 Yes or No

--- Weekend Excercises JS/Weekend JS1/Weekend JS.js
+++ Weekend Excercises JS/Weekend JS1/Weekend JS.js
@@ -11,3 +11,26 @@ const yesOrNo = {
         
 }
 }
+
+/* -------------------------------------------------------------------------- */
+/*                               Ex2.1 Yes or No                              */
+/* -------------------------------------------------------------------------- */
+
+function smallestElement(arr) {
+    let smallestElement1 = arr[0];
+    let smallestElement2 = arr[1];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < smallestElement1) {
+            smallestElement1 = arr[i];
+        }
+        } 
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < smallestElement2 && arr[i] > smallestElement1) {
+            smallestElement2 = arr[i];
+            }  
+    }      
+             
+    return (smallestElement1 + smallestElement2)
+}
