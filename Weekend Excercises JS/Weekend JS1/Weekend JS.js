14780779cce61aca3db3583b144e5eae3af01ef2
Yinononon <yinononon@gmail.com>
2023-09-26 10:20:00 +0300

    Ex2.2 - One and Zero

--- Weekend Excercises JS/Weekend JS1/Weekend JS.js
+++ Weekend Excercises JS/Weekend JS1/Weekend JS.js
@@ -34,3 +34,15 @@ function smallestElement(arr) {
              
     return (smallestElement1 + smallestElement2)
 }
+
+/* -------------------------------------------------------------------------- */
+/*                            Ex2.2 - One and Zero                            */
+/* -------------------------------------------------------------------------- */
+
+let n = 0
+function binToDec(array) {
+    for (let i = 0; i < array.length ; i++) {
+        n = n + (array[i])*(Math.pow(2, -1 + array.length - i ))    
+    }
+    return n ;
+}
