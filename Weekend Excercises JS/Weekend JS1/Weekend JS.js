20f88c9a4a8a2d5974ab65b8ed846ba320ad9d39
Yinononon <yinononon@gmail.com>
2023-09-26 10:25:38 +0300

    Ex2.4 Unique

--- Weekend Excercises JS/Weekend JS1/Weekend JS.js
+++ Weekend Excercises JS/Weekend JS1/Weekend JS.js
@@ -46,3 +46,23 @@ function binToDec(array) {
     }
     return n ;
 }
+
+/* -------------------------------------------------------------------------- */
+/*                                Ex2.4 Unique                                */
+/* -------------------------------------------------------------------------- */
+const arr = [0,0,0,0,0,0,0]
+let unique = arr[0];
+if ((arr[arr.length-1] !== arr[arr.length-2]) && (arr[arr.length-1] !== arr[arr.length-3] )) {
+    unique = arr[arr.length -1]
+}
+else if (arr[arr.length-2] !== arr[arr.length-3] && arr[arr.length-2] !== arr[arr.length-4]){
+    unique = arr[arr.length-2]
+}
+else{
+for (let i = 0; i < arr.length -2; i++) {
+    
+    if (arr[i] !== arr[i+1]  &&  arr[i] !== arr[i+2]) {
+        unique = arr[i]
+    }
+}    
+}
