f82ee9bb1d4e2bdb2e6bb9011bb648fe2379ef2c
Yinononon <yinononon@gmail.com>
2023-10-01 17:20:29 +0300

    Remove and Add method complit

--- Weekend 28.09/mession.js
+++ Weekend 28.09/mession.js
@@ -7,14 +7,15 @@ const carMarket =  require("./obj.js");
 
 const agencyOperations  = {
     search: function (agencyOrId) {
-            const ifAgency = carMarket.sellers.find((sellers) =>  sellers.agencyName === agencyOrId)
-            if(ifAgency){
-            console.log(ifAgency)
-            return ifAgency
+            const agency = carMarket.sellers.find((sellers) =>  sellers.agencyName === agencyOrId)
+            if(agency){
+            console.log(agency)
+            return agency
         }
             const ifId = carMarket.sellers.find(({agencyId}) => agencyOrId === agencyId)
             if(ifId){
-            return console.log(ifId)
+            console.log(ifId)
+            return ifId
             }
         return console.log("Agency not found plese enter a vaild agency name or agency ID")
         },
@@ -24,41 +25,52 @@ const agencyOperations  = {
         return console.log(allAgences)
      },
     addNewCar: function (agencyToAdd,carBrand,newCar) {
-        const ifAgency = carMarket.sellers.find((sellers) =>  sellers.agencyName === agencyToAdd)
-        if (ifAgency) {
-           let ifnewBrand = ifAgency.cars.find((cars) => cars.brand === carBrand)
-           if (!ifnewBrand) {
-            ifnewBrand = {
+        const agency = carMarket.sellers.find((sellers) =>  sellers.agencyName === agencyToAdd)
+        if (agency) {
+           let brand = agency.cars.find((cars) => cars.brand === carBrand)
+           if (!brand) {
+            brand = {
                 brand: carBrand,
                 models:[]
             };
-            console.log(ifAgency.cars.push(ifnewBrand))
+            agency.cars.push(brand)
             console.log(`New brand ${carBrand}`)
             };
-            ifnewBrand.models.unshift(newCar)
+            brand.models.unshift(newCar)
             console.log(`Added ${carBrand} - ${newCar.name} under ${agencyToAdd}`)
-          return;
         } 
         return "Some of the ditails wrong or missing pay attantion!" 
     },
-    removeCar: function (agencyTo,carBrand,removeCar) {
-        const ifAgency = carMarket.sellers.find((sellers) =>  sellers.agencyName === agencyTo)
-        const ifnewBrand = ifAgency.cars.find((cars) => cars.brand === carBrand)
-        delete ifnewBrand.models.find((models) => models.carNumber === removeCar)   
-        console.log(` ${carBrand} - ${newCar.name} under ${agencyToAdd} as removed`)
+    
+    removeCar: function (agencyToRemove,carBrand,carNumber) {
+        const agency = carMarket.sellers.find((sellers) =>  sellers.agencyName === agencyToRemove)
+        if (agency) {
+            const brand = agency.cars.find((cars) => cars.brand === carBrand)
+            if (brand) {
+                const carIndex = brand.models.findIndex((models) => models.carNumber === carNumber)
+                brand.models.splice(carIndex, 1)
+                console.log(` ${carBrand}, ${carNumber} form ${agencyToRemove} agency as removed`)
+            }
+        }
+       
     },
      
 }
 
 
-// car1 = {
-//     name: "007",
-//     year: 2222,
-//     price: 1137000,
-//     carNumber: "BBBBB",
-//     ownerId: "dude",
-//   }
-// agencyOperations.addNewCar('Best Deal',"DADA",car1)  
-// // agencyOperations.search('Best Deal')
-// // agencyOperations.retrieve()
+  
+car1 = {
+    name: "007",
+    year: 2222,
+    price: 1137000,
+    carNumber: "BBBBB",
+    ownerId: "dude",
+  }
+agencyOperations.addNewCar('Best Deal',"bDADA",car1);  
+
+// agencyOperations.retrieve()
 // console.log(carMarket.sellers[0].cars[5])
+
+console.log(carMarket.sellers[0].cars[5])
+agencyOperations.removeCar('Best Deal',"bDADA",'BBBBB')
+console.log(carMarket.sellers[0].cars[5])
